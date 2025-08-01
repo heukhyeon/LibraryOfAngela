124bf9eb5a02a3c02ba370e4f4d0dc1683cd5cb3
Heukhyeon, Philip <heukhyeon@gmail.com>
2025-08-01 16:07:13 +0900

    Update BattleUnitBuf_loaShield.cs

--- Interface/Buf/BattleUnitBuf_loaShield.cs
+++ Interface/Buf/BattleUnitBuf_loaShield.cs
@@ -80,6 +80,67 @@ public sealed class BattleUnitBuf_loaShield : BattleUnitBuf, IHandleChangeDamage
         controller.OnReduceStack(this, request);
     }
 
+    /// <summary>
+    /// 보호막을 수치 감소등의 이유 외의 이유로 소멸시킬때 호출합니다.
+    /// </summary>
+    /// <param name="attacker">호출자</param>
+    public void DestroyManually(BattleUnitModel attacker) {
+        controller.OnDestroyManually(this, attacker);
+    }
+}
+
+/// <summary>
+/// 보호막의 소멸의 이유를 알리는 클래스
+/// 대부분의 경우 일반 모드에서 사용하지마세요. 특정한 목적이 명확히 있을때만 <see cref="Etc"/>를 사용하세요.
+/// </summary>
+public class LoAShieldDestroyReason {
+    internal LoAShieldDestroyReason() {
+
+    }
+
+    /// <summary>
+    /// 피해로 인해 보호막 수치가 0이 되어서 파괴된 경우
+    /// </summary>
+    public class StackZero : LoAShieldDestroyReason {
+        /// <summary>
+        /// 스택이 0이 될때 마지막으로 피해를 입힌 대상
+        /// </summary>
+        public readonly BattleUnitModel attacker;
+
+        internal StackZero(BattleUnitModel attacker) {
+            this.attacker = attacker;
+        }
+    }
+
+    /// <summary>
+    /// 피해로 인해 보호막 수치가 0이 되어서 파괴된 경우
+    /// </summary>
+    public class RoundEnd : LoAShieldDestroyReason {
+        /// <summary>
+        /// 소멸 당시 기준 남은 보호막 수치. 기본적으로 보호막의 stack값과 동일합니다. 다른 모드에 의해 stack이 강제로 변경된 경우등을 고려해 별도 필드로 보존합니다.
+        /// </summary>
+        public readonly int remainStack;
+
+        internal RoundEnd(int remainStack) {
+            this.remainStack = remainStack;
+        }
+        
+    }
+
+    /// <summary>
+    /// 모드 등에서 임의로 파괴한경우. 프레임워크에서는 직접 부르지 않습니다.
+    /// </summary>
+    public class Etc : LoAShieldDestroyReason {
+        /// <summary>
+        /// 소멸을 호출한 대상
+        /// </summary>
+        public readonly BattleUnitModel attacker;
+
+        public Etc(BattleUnitModel attacker) {
+            this.attacker = attacker;
+        }
+    }
+    
 }
 
 /// <summary>
@@ -222,5 +283,7 @@ namespace LibraryOfAngela.Interface_Internal
         void OnReduceStack(BattleUnitBuf_loaShield buf, LoAShieldReduceRequest request);
 
         void OnRoundEnd(BattleUnitBuf_loaShield buf);
+
+        void OnDestroyManually(BattleUnitBuf_loaShield buf, BattleUnitModel attacker);
     }
 }
